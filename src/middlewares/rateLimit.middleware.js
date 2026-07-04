ea4fe0e5436709055f3844c60d96979ac50fc7ea
harisiskandarmuda20 <haris.iskandar@dayafnb.com>
2026-07-04 11:11:27 +0700

    fix: upgrade Rate limit to 10000

--- src/middlewares/rateLimit.middleware.js
+++ src/middlewares/rateLimit.middleware.js
@@ -2,7 +2,7 @@ const rateLimit = require('express-rate-limit');
 
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 500, // limit each IP to 500 requests per windowMs
+  max: 10000, // limit each IP to 10000 requests per windowMs
   message: {
     success: false,
     message: 'Too many requests from this IP, please try again in 15 minutes!'
